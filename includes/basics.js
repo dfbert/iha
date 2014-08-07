dba0f4f4e6efa2cf3e550cecbe253eaba344bc6b
dfbert <dfbert@live.com>
2014-08-07 20:47:36 -0300

    aa

--- includes/basics.js
+++ includes/basics.js
@@ -213,5 +213,8 @@ function onNotificationGCM(e) {
 
 }
 
-
+window.onerror = function(msg, url, linenumber) {
+    alert('Error message: '+msg+'\nURL: '+url+'\nLine Number: '+linenumber);
+    return true;
+}
 
