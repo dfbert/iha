d544d35c6293fc4ecd69669bafa3dd338b97a117
dfbert <dfbert@live.com>
2014-08-10 06:50:12 -0300

    aaa

--- me.js
+++ me.js
@@ -1,21 +1,4 @@
- window.onerror = function(msg, url, line, col, error) {
-   // Note that col & error are new to the HTML 5 spec and may not be 
-   // supported in every browser.  It worked for me in Chrome.
-   var extra = !col ? '' : '\ncolumn: ' + col;
-   extra += !error ? '' : '\nerror: ' + error;
-
-   // You can view the information in an alert to see things working like this:
-   alert("Error: " + msg + "\nurl: " + url + "\nline: " + line + extra);
-
-   // TODO: Report this error via ajax so you can keep track
-   //       of what pages have JS issues
-
-   var suppressErrorAlert = true;
-   // If you return true, then error alerts (like in older versions of 
-   // Internet Explorer) will be suppressed.
-   return suppressErrorAlert;
-};
- var socket = io('http://up1.dfbert.com:666', {transports: ['websocket']});
+var socket = io('http://up1.dfbert.com:666', {transports: ['websocket']});
   socket.on('auth', function (data) {
 	if(data.welcome != true){
 		logout();
@@ -28,8 +11,6 @@
 			aaa = 0;
 			}
 			aaa++;
-			alert(aaa);
-			alert(data.username.toUpperCase()+'_urb');
 			window.localStorage.setItem(window.__myusername+'_'+data.username.toUpperCase()+"_chatlogs_unread", aaa);
 			document.getElementById(data.username.toUpperCase()+'_urb').innerHTML=aaa;
 			delete aaa;
